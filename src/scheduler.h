1c61fcd3050486fb561d0d79be4e0aee948d44aa
Mohammed Q. Hussain <maastaar@gmail.com>
2021-09-16 23:37:35 +0300

    Fixing a mistake

--- src/scheduler.h
+++ src/scheduler.h
@@ -5,5 +5,5 @@ int next_sch_pid, curr_sch_pid;
 process_t *curr_process;
 
 void scheduler_init();
-void scheduler();
+void scheduler( int, int, int, int, int, int, int, int, int );
 void run_next_process();
