56103e45927185f8fdb2034c355887e09e8efe1c
Mohammed Q. Hussain <maastaar@gmail.com>
2021-11-15 20:44:29 +0300

    Changes for the text

--- src/ata.c
+++ src/ata.c
@@ -3,10 +3,6 @@
 // LBA
 void *read_disk( int address )
 {
-	void *buffer = kalloc( SECTOR_SIZE );
-	
-	// ... //
-	
 	dev_write( BASE_PORT + 6, ( 0x0e0 | ( ( address & 0x0F000000 ) >> 24 ) ) ); // Drive 0. Bits 0-3 = Bits 24-27 of LBA
 	dev_write( BASE_PORT + 2, 1 ); // Sector count
 	dev_write( BASE_PORT + 3, address & 0x000000FF ); // LBA's 0-7 bits
@@ -14,6 +10,8 @@ void *read_disk( int address )
 	dev_write( BASE_PORT + 5, ( address & 0x00FF0000 ) >> 16 ); // LBA's 16-23 bits
 	dev_write( BASE_PORT + 7, 0x20 ); // Command: Read with Retry
 	
+	// ... //
+		
 	int status = 0;
 	
 	do
@@ -23,7 +21,10 @@ void *read_disk( int address )
 	
 	// ... //
 	
-	ata_copy_to_buffer( BASE_PORT, SECTOR_SIZE / 2, buffer );
+	short *buffer = kalloc( SECTOR_SIZE );
+	
+	for ( int currByte = 0; currByte < ( SECTOR_SIZE / 2 ); currByte++ )
+		buffer[ currByte ] = dev_read( BASE_PORT );
 	
 	return buffer;
 }
